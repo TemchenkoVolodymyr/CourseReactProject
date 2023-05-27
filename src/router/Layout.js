46a655c7a642f8b348093295e64bcacb2f645635
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-05-27 22:34:00 +0200

    added sass

--- src/router/Layout.js
+++ src/router/Layout.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import Navigations from "./Navigations";
 
+
 const Layout = () => {
   return (
     <>
       <div>
-        <header>
+        <div>
           <Navigations />
-        </header>
+        </div>
         <div>
           <Outlet/>
         </div>
