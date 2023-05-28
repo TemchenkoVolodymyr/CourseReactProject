1f5065b201dce073612e1e6b77794cb5f9d23bf2
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-05-28 18:51:37 +0200

    created HomeHeader Component

---
+++ src/Components/Home/Header/HomeHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeHeader = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default HomeHeader;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomeHeader from "./Header/HomeHeader";
 
 const Home = () => {
   return (
-    <div>
-      HOME
-    </div>
+    <>
+      <HomeHeader />
+    </>
   );
 };
 
