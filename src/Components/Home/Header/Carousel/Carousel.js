772cf2df3734099d9f8dd81fdbea1de6ade622d8
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-05-28 20:55:42 +0200

    finished simple carousel

--- src/Components/Home/Header/Carousel/Carousel.js
+++ src/Components/Home/Header/Carousel/Carousel.js
@@ -1,34 +1,42 @@
 import React, {Children, cloneElement, useEffect, useState} from 'react';
 import style from "./Carousel.module.scss"
-import {FaChevronLeft} from "react-icons/fa";
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 
 const Carousel = ({children}) => {
 
   const [pages, setPages] = useState([])
+  const [offset, setOffset] = useState(0)
 
-  // useEffect(() => {
-  //   setPages(
-  //     Children.map(children,(child) => {
-  //       console.log(child)
-  //       return cloneElement(child,{
-  //         style:{
-  //           height:"100%",
-  //           minWidth:"100%",
-  //           maxWidth:"100%"
-  //         },
-  //       })
-  //     })
-  //   )
-  // }, [])
+
+  const handleRightClick = () => {
+    setOffset((currentOffset) => {
+        const newOffset = currentOffset + 420
+      console.log(currentOffset)
+      return Math.min(newOffset,0)
+  }
+    )}
+
+  const handleLeftClick = () => {
+    console.log('a')
+    setOffset((currentOffset) => {
+      const newOffset = currentOffset - 420
+      const maxOffset = -(420 * (children.length -1))
+
+      return Math.max(newOffset,maxOffset)
+    })
+  }
+
 
   return (
     <div className={style.container}>
-      <FaChevronLeft></FaChevronLeft>
+      <FaChevronLeft className={style.arrow} onClick={handleLeftClick}></FaChevronLeft>
+
       <div className={style.window}>
-        <div className={style.items}>
+        <div className={style.items} style={{transform:`translateX(${offset}px)`}}>
           {children}
         </div>
       </div>
+      <FaChevronRight className={style.arrow} onClick={handleRightClick}></FaChevronRight>
 
     </div>
   );
