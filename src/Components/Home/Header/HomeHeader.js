750d73ba27ef35d21ba08bbeb4e50ae54a8ff127
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-05-28 19:22:07 +0200

    started make Home Header slider  and created assets folder

--- src/Components/Home/Header/HomeHeader.js
+++ src/Components/Home/Header/HomeHeader.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import style from "./HomeHeader.module.scss"
+
 
 const HomeHeader = () => {
   return (
-    <div>
-
+    <div className={style.container}>
+      <div className={style.main}>
+          <h3>ENCANTO</h3>
+          <h4>Cartoons Comedy</h4>
+          <button>Watch</button>
+      </div>
     </div>
   );
 };
