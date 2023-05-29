d43871b2139af54e9396468ea8fb70889b4204ac
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-05-29 20:15:48 +0200

    added bestActors to initialStore

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,45 +1,71 @@
 export const initialStore = {
-    header: 'Hello, i`m from initialStore',
-    reduxTestCounter: 0,
-    mediaJSON:null,
+  header: 'Hello, i`m from initialStore',
+  reduxTestCounter: 0,
+  mediaJSON: null,
 
-    headerHomeMovies:[
-        {
-            name:"ENCANTO",
-            category:"Cartoons Comedy",
-            id:1,
-            url:"https://ernoticias.com/wp-content/uploads/2021/08/Screenshot_163-735x400.jpg"
-        },
-        {
-            name:"MAD-MAX",
-            category:"Thriller",
-            id:2,
-            url:"https://i.ytimg.com/vi/DJgq33-4QbQ/maxresdefault.jpg"
-        },
-        {
-            name:"AVATAR 2",
-            category:"Fantasy",
-            id:3,
-            url:"https://th.bing.com/th/id/R.159ce384dbca5ec67f82fbecb34d79da?rik=uf4XgukBaMr2mQ&pid=ImgRaw&r=0"
-        },
-        {
-            name:"Hobbit",
-            category:"Fantasy",
-            id:4,
-            url:"https://th.bing.com/th/id/OIP.98cXjhTd3sXNUEpf4ds3nAHaEo?pid=ImgDet&rs=1"
-        },
-        {
-            name:"SPIDER-MAN",
-            category:"Fantasy",
-            id:5,
-            url:"https://th.bing.com/th/id/R.082aa85eadb387dc978e8e8b6106eeb1?rik=Zs5S67H60QOF6g&pid=ImgRaw&r=0"
-        },
-        {
-            name:"JOHN WICK",
-            category:"Thriller",
-            id:6,
-            url:"https://th.bing.com/th/id/R.20b1561366dc8453d3d02900cd6c1832?rik=tD36mxYKYrvAsw&pid=ImgRaw&r=0"
-        }
-    ]
+  headerHomeMovies: [
+    {
+      name: "ENCANTO",
+      category: "Cartoons Comedy",
+      id: 1,
+      url: "https://ernoticias.com/wp-content/uploads/2021/08/Screenshot_163-735x400.jpg"
+    },
+    {
+      name: "MAD-MAX",
+      category: "Thriller",
+      id: 2,
+      url: "https://i.ytimg.com/vi/DJgq33-4QbQ/maxresdefault.jpg"
+    },
+    {
+      name: "AVATAR 2",
+      category: "Fantasy",
+      id: 3,
+      url: "https://th.bing.com/th/id/R.159ce384dbca5ec67f82fbecb34d79da?rik=uf4XgukBaMr2mQ&pid=ImgRaw&r=0"
+    },
+    {
+      name: "Hobbit",
+      category: "Fantasy",
+      id: 4,
+      url: "https://th.bing.com/th/id/OIP.98cXjhTd3sXNUEpf4ds3nAHaEo?pid=ImgDet&rs=1"
+    },
+    {
+      name: "SPIDER-MAN",
+      category: "Fantasy",
+      id: 5,
+      url: "https://th.bing.com/th/id/R.082aa85eadb387dc978e8e8b6106eeb1?rik=Zs5S67H60QOF6g&pid=ImgRaw&r=0"
+    },
+    {
+      name: "JOHN WICK",
+      category: "Thriller",
+      id: 6,
+      url: "https://th.bing.com/th/id/R.20b1561366dc8453d3d02900cd6c1832?rik=tD36mxYKYrvAsw&pid=ImgRaw&r=0"
+    }
+  ],
+  bestActors: [
+    {
+      name: "Thomas Holland",
+      countOfFilms: +1,
+    },
+    {
+      name: "Stephan Holland",
+      countOfFilms: +1,
+},
+    {
+      name: "Simu Liu",
+      countOfFilms: +1,
+    },
+    {
+      name: "Hao Nicko",
+      countOfFilms: +1,
+    },
+    {
+      name: "Queen Toq",
+      countOfFilms: +2,
+    },
+    {
+      name: "Prince Jack",
+      countOfFilms: +5,
+    }
+  ]
 
 };
