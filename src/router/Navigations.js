cc3c901e4b821d1def8a0b63631f34504dee2936
Serhi Hats <gatsserv@gmail.com>
2023-05-29 21:52:24 +0300

    fix change color icon for nav

--- src/router/Navigations.js
+++ src/router/Navigations.js
@@ -5,17 +5,7 @@ import {Icon} from "../Components/Icon/Icon";
 
 const Navigations = () => {
 
-  const [active, setActive] = useState(false);
   const [login, setLogin] = useState(false);
-
-  const isPush = ({isActive}) => {
-    if (isActive) {
-      setActive(true);
-      return "active";
-    }
-    setActive(false);
-    return "";
-  }
 
   const isLogin = ({isActive}) => {
     if (isActive) {
@@ -28,21 +18,22 @@ const Navigations = () => {
 
   return (
     <>
-      <NavLink to="/"><Icon type={iconTypes.film} size={"30px"} color={"#E30914"}/><h1>MovieMagic.UA</h1></NavLink>
+      <NavLink to="/"><Icon type={iconTypes.film} size={"30px"} color={"#E30914"}/>
+        <h1>MovieMagic<span>.</span>UA</h1>
+      </NavLink>
       <p>menu</p>
-      <NavLink className={isPush} to="/"> <Icon type={iconTypes.home} size={"20px"}
-                                                color={active ? "#E30914" : "grey"}/>Home</NavLink>
-      <NavLink to='/discovery'><Icon type={iconTypes.compass} size={"20px"} color={"grey"}/>Discovery</NavLink>
-      <NavLink to='/fresh'><Icon type={iconTypes.spinner} size={"20px"} color={"grey"}/>Fresh movies</NavLink>
-      <NavLink to='/trending'><Icon type={iconTypes.fire} size={"20px"} color={"grey"}/>Trending now</NavLink>
+      <NavLink to="/"> <Icon type={iconTypes.home}/>Home</NavLink>
+      <NavLink to='/discovery'><Icon type={iconTypes.compass}/>Discovery</NavLink>
+      <NavLink to='/fresh'><Icon type={iconTypes.spinner}/>Fresh movies</NavLink>
+      <NavLink to='/trending'><Icon type={iconTypes.fire}/>Trending now</NavLink>
       <p>popular genders</p>
-      <NavLink to='/comedy'><Icon type={iconTypes.happy2} size={"20px"} color={"grey"}/>Comedy</NavLink>
-      <NavLink to='/cartoons'><Icon type={iconTypes.manWomen} size={"20px"} color={"grey"}/>Cartoons</NavLink>
-      <NavLink to='/fantasy'><Icon type={iconTypes.magicWand} size={"20px"} color={"grey"}/>Fantasy</NavLink>
-      <NavLink to='/biography'><Icon type={iconTypes.hipster2} size={"20px"} color={"grey"}/>Biography</NavLink>
+      <NavLink to='/comedy'><Icon type={iconTypes.happy2}/>Comedy</NavLink>
+      <NavLink to='/cartoons'><Icon type={iconTypes.manWomen}/>Cartoons</NavLink>
+      <NavLink to='/fantasy'><Icon type={iconTypes.magicWand}/>Fantasy</NavLink>
+      <NavLink to='/biography'><Icon type={iconTypes.hipster2}/>Biography</NavLink>
       <p>general</p>
-      {login ? <NavLink className={isLogin} to='/auth'><Icon type={iconTypes.exit} size={"20px"} color={"grey"}/>Logout</NavLink> :
-        <NavLink to='/auth'><Icon type={iconTypes.enter} size={"20px"} color={"grey"}/>Login</NavLink>
+      {login ? <NavLink className={isLogin} to='/auth'><Icon type={iconTypes.exit}/>Logout</NavLink> :
+        <NavLink className={isLogin} to='/auth'><Icon type={iconTypes.enter}/>Login</NavLink>
       }
     </>
   );
