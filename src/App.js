0acbca7f0edf0a8da3a82504f70e889849a81e6c
Kateryna Laina <boba4ok@gmail.com>
2023-06-02 17:52:28 +0300

    merge setup_api branch with dev

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import ReduxTestCounter from './Components/reduxTestCounter/ReduxTestCounter';
 import {useDispatch,} from "react-redux";
 import {useEffect} from "react";
 import {jsonAC} from "./redux/json/jsonActions";
-
 import data from "./redux/json/Data.json"
 import Home from "./Components/Home/Home";
 import NotfoundPage from "./pages/NotfoundPage";
 import MovieCarousel from "./Components/Home/Header/Carousel/MovieCarouselLink/MovieCarousel";
-
 import CurrentMovie from "./Components/Home/Popular Movies/CurrentMovie";
 import PopularMoviesPage from "./Components/Home/Popular Movies/PopularMoviesPage/PopularMoviesPage";
 import AdminPanel from "./Components/AdminPanel/AdminPanel";
-
 import { getAuth, onAuthStateChanged } from "firebase/auth"
-import {removeUser, setUser} from "./redux/slices/userSlice";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import StatisticsAP from "./Components/AdminPanel/NavComponents/StatisticsAP/StatisticsAP";
 import UsersAP from "./Components/AdminPanel/NavComponents/UsersAP/UsersAP";
 import MoviesAP from "./Components/AdminPanel/NavComponents/MoviesAP";
 import ActorsAP from "./Components/AdminPanel/NavComponents/ActorsAP";
 import GenresAP from "./Components/AdminPanel/NavComponents/GenresAP";
+import {removeUser, setUser} from "./redux/slices/userSlice";
+
 
 function App() {
 
@@ -77,9 +75,9 @@ function App() {
             <Route path="actors" element={<ActorsAP />}/>
             <Route path="genres" element={<GenresAP />}/>
           </Route>
+
         </Route>
       </Routes>
-
     </>
   );
 }
