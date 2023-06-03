dc50af4ebbb3f6e78a16f3275eabe7e6da7483e9
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-06-03 10:06:37 +0200

    fix sersAPI

--- src/Components/AdminPanel/NavComponents/UsersAP/UsersAP.js
+++ src/Components/AdminPanel/NavComponents/UsersAP/UsersAP.js
@@ -7,29 +7,43 @@ import {collection, getDocs, getFirestore} from "firebase/firestore";
 const UsersAP = () => {
 
 // search
-  let [foundEmail, setFoundEmail] = useState([]);
+  let [searchData, setSearchData] = useState([]);
 
-  let usersAp = useSelector((store) => store.usersAp);
+  const [users, setUsers] = useState([]);
 
-  const searchMovie = (foundItem) => foundItem && usersAp.filter(item => item.email.toLowerCase().includes(foundItem.toLowerCase()))
+  const searchMovie = (foundItem) => foundItem && users.filter(item => item.email.toLowerCase().includes(foundItem.toLowerCase()))
 
 
 // emails section
 
-
-  let email = usersAp.map(item => <li>{item.email}</li>);
-
-  let date = usersAp.map(item => <li>{item.date}</li>)
-
-  let action = usersAp.map(item => <li>{usersAp.length}</li>)
+  let foundEm = searchData &&searchData.map(item => <li>{item.email}</li>)
+  let foundDate = searchData && searchData.map(item => <li>{item.date}</li>)
+  let foundAction = searchData && searchData.map(item => <li>{item.action}</li>)
 
 
-  let foundEm = foundEmail &&foundEmail.map(item => <li>{item.email}</li>)
-  let foundDate = foundEmail && foundEmail.map(item => <li>{item.date}</li>)
-  let foundAction = foundEmail && foundEmail.map(item => <li>{item.action}</li>)
+  const fetchUsers = async () => {
+    const db = getFirestore();
 
-  let x = () => {
-    if (foundEmail) {
+    // Получите все документы из коллекции 'users'
+    const userSnapshot = await getDocs(collection(db, "users"));
+
+    // Преобразуйте каждый документ в данные пользователя и верните их
+    const users = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    return users;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then(fetchedUsers => setUsers(fetchedUsers))
+      .catch(error => console.error(error));
+  }, []);
+
+  let emailDataBase = users.map(user => <li>{user.email}</li>)
+  let dateDataBase = users.map(user => <li>{user.date && user.date.toString()}</li>)
+
+  let ifSearchActive = () => {
+    if (searchData) {
       return (
         <>
           <div className={style.container}>
@@ -50,71 +64,33 @@ const UsersAP = () => {
       )
     } else {
       return (
-        // let user = users.map(user =>
-        // <ul>
-        //   {/*<li>{user.email}</li>*/}
-        //   {foundEm}
-        // </ul>
-
         <>
-
-            {users.map(user => <>
             <div className={style.container}>
               <ul>
                 <li>Email</li>
-                {user.email}
+                {emailDataBase}
               </ul>
               <ul>
                 <li>Data Register</li>
-                { user.date && user.date.toString()}
+                {dateDataBase}
               </ul>
               <ul>
                 <li>Actions</li>
-                {action}
+                2
               </ul>
             </div>
-              </>
-            )}
-
-
         </>
       );
     }
   }
 
 
-
-  const [users, setUsers] = useState([]);
-  let [data,setData] = useState([]);
-
-  const fetchUsers = async () => {
-    const db = getFirestore();
-
-    // Получите все документы из коллекции 'users'
-    const userSnapshot = await getDocs(collection(db, "users"));
-
-    // Преобразуйте каждый документ в данные пользователя и верните их
-    const users = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-    return users;
-  }
-
-  useEffect(() => {
-    fetchUsers()
-      .then(fetchedUsers => setUsers(fetchedUsers))
-      .catch(error => console.error(error));
-  }, []);
-
-  console.log(users)
   return (
     <>
     <div>
-      <UniversalSearch callback={searchMovie} found={foundEmail} setFound={setFoundEmail}/>
+      <UniversalSearch callback={searchMovie} found={searchData} setFound={setSearchData}/>
     </div>
-      <div>
-        {/*{user}*/}
-      </div>
-  {x()}
+  {ifSearchActive()}
     </>
   )
 };
