c08e81e9a9bd44f570af742589084182f13b4ffa
Kateryna Laina <boba4ok@gmail.com>
2023-06-10 18:32:07 +0300

    remove AuthReducer.js

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -5,7 +5,6 @@ import {jsonReducer} from "./json/jsonReducer";
 import {reducerHomeHeader} from "../Components/Home/Header/reducerHomeHeader";
 import user from './slices/userSlice'
 import movies from './slices/movieSlice'
-import {authReducer} from "../Components/Auth/login/AuthReducer";
 import {statisticsReducer} from "../Components/AdminPanel/NavComponents/StatisticsAP/statisticsReducer";
 import {usersReducerAp} from "../Components/AdminPanel/NavComponents/UsersAP/usersReducerAP";
 import {searchReducer} from "../Components/Home/Search/SearchReducer";
@@ -17,7 +16,6 @@ export default combineReducers({
     jsonDataMedia: jsonReducer,
     headerMovies: reducerHomeHeader,
     user,
-    isAuth: authReducer,
     statistics: statisticsReducer,
     movies,
     usersAp: usersReducerAp,
