85ce6aa45d4fd6ca0e26a200b4a5d40b8310b19d
temcenkovolo <temchenkovolo@gmail.com>
2023-06-11 20:44:41 +0200

    added for AuthForm another component Form

--- src/Components/Auth/login/AuthForm.jsx
+++ src/Components/Auth/login/AuthForm.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import styles from './AuthForm.module.scss'
 import {useForm} from "react-hook-form";
 import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
@@ -6,7 +6,8 @@ import {useDispatch} from "react-redux";
 import {setUser} from "../../../redux/slices/userSlice";
 import {useNavigate} from "react-router";
 import {useAuth} from "../../../hooks/useAuth";
-import {getFirestore, doc, setDoc, getDocs, collection, getDoc} from "firebase/firestore";
+import {getFirestore, doc, setDoc,getDoc} from "firebase/firestore";
+import Form from "./Form";
 
 const AuthForm = () => {
   const {isAuth} = useAuth()
@@ -103,53 +104,11 @@ const AuthForm = () => {
   return (
     <section className={styles.wrapperBox}>
       <h1>Auth</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="userName">e-mail</label>
-        <input
-          {...register('email', {
-              required: 'Email is required',
-              pattern: {
-                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                message: 'Please enter a valid email '
-              }
-            }
-          )}
-          type="text"
-          placeholder='Enter your email'
-        />
-        {errors.email && <div className={styles.errorMessage}>{errors.email.message}</div>}
-        <label htmlFor="userName">password</label>
-        <input
-          {...register('password', {
-              required: 'Password is required',
-              minLength: {
-                value: 6,
-                message: 'Password should contain more then 6 characters '
-              },
-              pattern: {
-                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                message: 'Password should contain at least one letter and one number'
-              }
-            }
-          )}
-          type="password"
-          placeholder='Enter your password'
-        />
-        {errors.password && <div className={styles.errorMessage}>{errors.password.message}</div>}
-        {error && <div className={styles.errorMessage}>{error}</div>}
-        <div className={styles.btnContainer}>
-          <button
-            className={styles.active}
-            type='button'
-            onClick={handleSubmit(loginHandler)}
-          >Login
-          </button>
-          <button
-            onClick={handleSubmit(registerHandler)}
-            type='button'>Register
-          </button>
-        </div>
-      </form>
+      <Form submit={handleSubmit} onSubmit={onSubmit}
+            register={register} errors={errors}
+            error={error} registerHandler={registerHandler}
+            loginHandler={loginHandler}>
+      </Form>
     </section>
   );
 };
