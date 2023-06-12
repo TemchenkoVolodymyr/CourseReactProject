0bb813a75b51f23e2677c5fed48cf92d724627eb
temcenkovolo <temchenkovolo@gmail.com>
2023-06-12 16:54:14 +0200

    refactored redux folder

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -2,18 +2,15 @@ import {combineReducers} from "redux";
 import user from './slices/userSlice'
 import movies from './slices/movieSlice'
 import {statisticsReducer} from "../Components/AdminPanel/NavComponents/AdminPanelStatistics/statisticsReducer";
-import {usersReducerAp} from "../Components/AdminPanel/NavComponents/AdminPanelUsers/usersReducerAP";
 import {searchReducer} from "../Components/Search/SearchReducer";
 import {authReducer} from "../Components/Auth/AuthReducer";
 
 
-
 export default combineReducers({
-    user,
-    isAuth: authReducer,
-    statistics: statisticsReducer,
-    movies,
-    usersAp: usersReducerAp,
-  searchMovie: searchReducer,
-  },
+        user,
+        isAuth: authReducer,
+        statistics: statisticsReducer,
+        movies,
+        searchMovie: searchReducer,
+    },
 );
