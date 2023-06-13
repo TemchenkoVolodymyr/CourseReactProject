9097fe7152d935c3c36c5cfe8bb42f7c1d03d90f
temcenkovolo <temchenkovolo@gmail.com>
2023-06-13 06:50:32 +0200

    added nav to genres

--- src/pages/MoviePage/MoviePage.js
+++ src/pages/MoviePage/MoviePage.js
@@ -4,139 +4,146 @@ import style from "./MoviePage.module.scss";
 import axios from "axios";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {NavLink} from "react-router-dom";
-import { Navigation } from 'swiper';
+import {Navigation} from 'swiper';
 import CircleRating from "../../Components/CircleRating/CircleRating";
+
 const MoviePage = () => {
-  const {id} = useParams();
-  const [movie, setMovie] = useState();
-  const imageBaseUrl = 'https://image.tmdb.org/t/p/'
-  useEffect(() => {
-    async function fetchMovie() {
-      try {
-        const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos,credits,similar`);
-        setMovie(data);
-      } catch (err) {
-        alert('Error');
-      }
+    const {id} = useParams();
+    const [movie, setMovie] = useState();
+    const imageBaseUrl = 'https://image.tmdb.org/t/p/'
+    useEffect(() => {
+        async function fetchMovie() {
+            try {
+                const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos,credits,similar`);
+                setMovie(data);
+            } catch (err) {
+                alert('Error');
+            }
+        }
+
+        fetchMovie();
+
+    }, [id]);
+
+    if (!movie) {
+        return <>Loading....</>;
     }
-    fetchMovie();
 
-  }, [id]);
+    let genres = movie.genres.map(genre => <NavLink to={`/${genre.name.toLowerCase()
+        .replace(/\s+/g, '-')}`}>{genre.name.split(' ').join(',')}</NavLink>)
+    return (
+        <>
+            <div className={style.wrapper}>
+                <div
+                    style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}}
+                    className={style.banner}>
+                    <div className={style.info}>
+                        <h1>{movie.title}</h1>
+                        <p>{movie.release_date.substring(0, 4)} - {movie.production_countries.map(country => country.iso_3166_1).join(', ')} - {movie.runtime} min</p>
 
-  if (!movie) {
-    return <>Loading....</>;
-  }
+                        <p ><span>Genres:</span> <span className={style.genres}>{genres} </span></p>
 
-  return (
-    <>
-      <div className={style.wrapper}>
-        <div
-          style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}}
-          className={style.banner}>
-          <div className={style.info}>
-            <h1>{movie.title}</h1>
-            <p>{movie.release_date.substring(0, 4)} - {movie.production_countries.map(country => country.iso_3166_1).join(', ')} - {movie.runtime} min</p>
-            <p><span>Genres:</span> {movie.genres.map(genre => genre.name).join(', ')}</p>
-            <p><span>Actors:</span> Thomas Holland</p>
-          </div>
-        </div>
-        <h2>Overview</h2>
-
-        <div className={style.overview}>
-          <div className={style.rating}>
-            <p>User Score</p>
-            <CircleRating
-              rating={movie.vote_average * 10}
-              size={110}
-            />
-          </div>
-          <div className={style.about}>
-            <p>{movie.overview}
-             </p>
-          </div>
-          <div className={style.data}>
-            <p><strong>Release Date:</strong> {movie.release_date}</p>
-            <p><strong>Original Language:</strong> {movie.original_language.toUpperCase()}</p>
-            <p><strong>Budget:</strong> $ {movie.budget}</p>
-            <p><strong>Revenue:</strong> $ {movie.revenue}</p>
-
-          </div>
-        </div>
-
-        <div>
-          <h2>Top Billed Cast</h2>
-          <Swiper
-            modules={[Navigation]}
-            id="main"
-            tag="section"
-            wrapperTag="ul"
-            navigation
-            slidesPerView={5}
-            spaceBetween={10}>
-
-            {movie?.credits.cast.slice(0,20).map(actor =>
-                  actor.profile_path &&
-                  <SwiperSlide key={actor.id}>
-                  <div className={style.actors}>
-                    <img
-                      src={`${imageBaseUrl}w200${actor.profile_path}`}
-                      alt={actor.name}/>
-                    <h3>{actor.name}</h3>
-                    <p>{actor.character}</p>
-                  </div>
-                  </SwiperSlide>
-            )}
-          </Swiper>
-
-        </div>
-
-        <div>
-          <h2>What the Trailer</h2>
-          {movie.videos.results.length > 0 && (
-            <div>
-              {movie?.videos.results
-                .filter(video => video.type === "Trailer")
-                .slice(0, 1)
-                .map(video => (
-                  <div key={video.key}>
-                    <iframe
-                      width="100%"
-                      height="500"
-                      src={`https://www.youtube.com/embed/${video.key}`}
-                      title={video.name}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    ></iframe>
-                  </div>
-                ))}
-            </div>
-          )}
-        </div>
-        <h2>Similar</h2>
-        <Swiper
-          modules={[Navigation]}
-          id="main"
-          tag="section"
-          wrapperTag="ul"
-          navigation slidesPerView={15}
-          spaceBetween={10}>
-
-          {movie?.similar.results.map(similar =>
-            similar.poster_path &&
-            <SwiperSlide key={similar.id}>
-              <NavLink to={`/movie/${similar.id}`}>
-                <div className={style.similar}>
-                  <img src={`https://image.tmdb.org/t/p/w200/${similar.poster_path}`} alt={similar.title}/>
+                        <p><span>Actors:</span> Thomas Holland</p>
+                    </div>
                 </div>
-              </NavLink>
-            </SwiperSlide>
-          )}
-        </Swiper>
+                <h2>Overview</h2>
 
-      </div>
-    </>
-  );
+                <div className={style.overview}>
+                    <div className={style.rating}>
+                        <p>User Score</p>
+                        <CircleRating
+                            rating={movie.vote_average * 10}
+                            size={110}
+                        />
+                    </div>
+                    <div className={style.about}>
+                        <p>{movie.overview}
+                        </p>
+                    </div>
+                    <div className={style.data}>
+                        <p><strong>Release Date:</strong> {movie.release_date}</p>
+                        <p><strong>Original Language:</strong> {movie.original_language.toUpperCase()}</p>
+                        <p><strong>Budget:</strong> $ {movie.budget}</p>
+                        <p><strong>Revenue:</strong> $ {movie.revenue}</p>
+
+                    </div>
+                </div>
+
+                <div>
+                    <h2>Top Billed Cast</h2>
+                    <Swiper
+                        modules={[Navigation]}
+                        id="main"
+                        tag="section"
+                        wrapperTag="ul"
+                        navigation
+                        slidesPerView={5}
+                        spaceBetween={10}>
+
+                        {movie?.credits.cast.slice(0, 20).map(actor =>
+                            actor.profile_path &&
+                            <SwiperSlide key={actor.id}>
+                                <div className={style.actors}>
+                                    <img
+                                        src={`${imageBaseUrl}w200${actor.profile_path}`}
+                                        alt={actor.name}/>
+                                    <h3>{actor.name}</h3>
+                                    <p>{actor.character}</p>
+                                </div>
+                            </SwiperSlide>
+                        )}
+                    </Swiper>
+
+                </div>
+
+                <div>
+                    <h2>What the Trailer</h2>
+                    {movie.videos.results.length > 0 && (
+                        <div>
+                            {movie?.videos.results
+                                .filter(video => video.type === "Trailer")
+                                .slice(0, 1)
+                                .map(video => (
+                                    <div key={video.key}>
+                                        <iframe
+                                            width="100%"
+                                            height="500"
+                                            src={`https://www.youtube.com/embed/${video.key}`}
+                                            title={video.name}
+                                            frameBorder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowFullScreen
+                                        ></iframe>
+                                    </div>
+                                ))}
+                        </div>
+                    )}
+                </div>
+                <h2>Similar</h2>
+                <Swiper
+                    modules={[Navigation]}
+                    id="main"
+                    tag="section"
+                    wrapperTag="ul"
+                    navigation slidesPerView={15}
+                    spaceBetween={10}>
+
+                    {movie?.similar.results.map(similar =>
+                        similar.poster_path &&
+                        <SwiperSlide key={similar.id}>
+                            <NavLink to={`/movie/${similar.id}`}>
+                                <div className={style.similar}>
+                                    <img src={`https://image.tmdb.org/t/p/w200/${similar.poster_path}`}
+                                         alt={similar.title}/>
+                                </div>
+                            </NavLink>
+                        </SwiperSlide>
+                    )}
+                </Swiper>
+
+            </div>
+        </>
+    );
 };
 
 export default MoviePage;
