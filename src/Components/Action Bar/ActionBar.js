2f242b8428dd3d7f8031d26d05952f45a21e276b
Kateryna Laina <boba4ok@gmail.com>
2023-06-14 18:17:04 +0300

    styling tooltip

--- src/Components/Action Bar/ActionBar.js
+++ src/Components/Action Bar/ActionBar.js
@@ -16,39 +16,39 @@ const ActionBar = () => {
           size={30}
           data-tooltip-id="list"
           data-tooltip-content="Add to list"
-          data-tooltip-place="right"
         />}/>
         <ActionButton icon={<AiFillHeart
           size={30}
           data-tooltip-id="like"
           data-tooltip-content="Mark as favorite"
-          data-tooltip-place="right"
         />}/>
         <ActionButton icon={<BsFillBookmarkFill
           size={25}
           data-tooltip-id="watchlist"
           data-tooltip-content="Add to yout watchlist"
-          data-tooltip-place="right"
         />}/>
         <ActionButton icon={<AiFillStar
           size={30}
           data-tooltip-id="rate"
           data-tooltip-content="Rate it"
-          data-tooltip-place="right"
         />}/>
       </div>
       <Tooltip
         id="list"
-        className={styles.tooltip}/>
+        className={styles.tooltip}
+        place="bottom"/>
       <Tooltip
         id="like"
-        className={styles.tooltip}/>
+        className={styles.tooltip}
+        place="bottom"/>
       <Tooltip
         id="watchlist"
-        className={styles.tooltip}/>
+        className={styles.tooltip}
+        place="bottom"/>
       <Tooltip
         id="rate"
-        className={styles.tooltip}/>
+        className={styles.tooltip}
+        place="bottom"/>
     </div>
   );
 };
