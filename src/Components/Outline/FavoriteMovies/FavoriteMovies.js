b107f24f2655e7cbd348796c275a395615215838
Kateryna Laina <boba4ok@gmail.com>
2023-06-19 21:17:45 +0300

    feature - Favorite Sidebar Component

--- src/Components/Outline/FavoriteMovies/FavoriteMovies.js
+++ src/Components/Outline/FavoriteMovies/FavoriteMovies.js
@@ -1,69 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { getAuth } from 'firebase/auth';
-import axios from 'axios';
+import React, { useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
 import style from '../Outline.module.scss';
 import CircleRating from '../../CircleRating/CircleRating';
 import CustomButton from '../../Button/CustomButton';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchFavorites} from "../../../redux/slices/favoriteSlice";
 
 
 
 const FavoriteMovies = () => {
 
-    const [favoriteMovies, setFavoriteMovies] = useState();
-    const auth = getAuth();
-
-    useEffect(() => {
-      async function fetchMovie() {
-        try {
-          const { data } = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos,credits,similar`);
-          setFavoriteMovies(data.results);
-        } catch (err) {
-          alert('Error');
-        }
-      }
-
-      fetchMovie();
-    }, []);
-
-    const imageBaseUrl = 'https://image.tmdb.org/t/p/';
-
-    const showFavoriteMovies = favoriteMovies && favoriteMovies.slice(0, 4).map((movie) => <NavLink
-      key={movie.id}
-      to={`/movie/${movie.id}`}
-    >
-      <div
-        style={{
-          backgroundImage: `url(${imageBaseUrl}w500${movie.poster_path})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover'
-        }}
-        className={style.wrapperBox}>
-
-        <div className={style.wrapperAbout}>
-          <div>
-            <h3 className={style.item}>{movie.original_title}</h3>
-            <p className={style.item}>{movie.category}</p></div>
-          <div>
-            <CircleRating
-              rating={movie.vote_average * 10}
-              displayAsPercentage={true}
-              size={70}/>
-          </div>
-        </div>
-      </div>
-    </NavLink>);
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.user.id);
+  const favorites = useSelector((state) => state.favorites.favorites);
+  const isLoading = useSelector((state) => state.favorites.isLoading);
+  console.log(favorites);
 
 
-    if (auth && auth._currentUser && auth._currentUser.length > 1) {
-      return <p>To see your favorite movies just log in</p>;
+  useEffect(() => {
+    if (isLoading === 'idle' && userId) {
+      dispatch(fetchFavorites(userId));
     }
+  }, [isLoading, userId]);
+
     return (
       <div>
         <h3>FAVORITE</h3>
-        {showFavoriteMovies}
+        {favorites && favorites.slice(0, 5).map((movie) =>
+          <NavLink
+            key={movie.id}
+            to={`/movie/${movie.movieInfo.id}`}
+          >
+            <div
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.movieInfo.poster_path})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover'
+              }}
+              className={style.wrapperBox}>
+
+              <div className={style.wrapperAbout}>
+                <div>
+                  <h3 className={style.item}>{movie.movieInfo.original_title}</h3>
+                  <p className={style.item}>{movie.movieInfo.category}</p></div>
+                <div>
+                  <CircleRating
+                    rating={movie.movieInfo.vote_average * 10}
+                    displayAsPercentage={true}
+                    size={70}/>
+                </div>
+              </div>
+            </div>
+          </NavLink>)}
+
         <div className={style.btn}>
-          <CustomButton name={'see more'} path={'favoriteMovies'}></CustomButton>
+          <CustomButton name={'see more'} path={'/u/favorites'}></CustomButton>
         </div>
       </div>
     );
