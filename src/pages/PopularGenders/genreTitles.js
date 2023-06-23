c8f381df30b02e851320a4aa94188236e29c24bd
Kateryna Laina <boba4ok@gmail.com>
2023-06-23 12:46:36 +0300

    feature - implement titles for HomePAge and Genre Pages

---
+++ src/pages/PopularGenders/genreTitles.js
@@ -0,0 +1,24 @@
+const genreTitles = {
+  comedy: 'Comedy Movies: Unleash Laughter with Our Extensive List of Humorous Films',
+  cartoons: 'Ultimate List of Must-See Cartoon Movies',
+  fantasy: 'Discover the Best Fantasy Movies: Ultimate List of Magical Films ',
+  documentary: 'Documentaries: Comprehensive List of Must-Watch Documentary Films',
+  adventure: 'Adventure Movies: Comprehensive List of Must-Watch Films',
+  animation: 'Ultimate List of Animation Movies: Explore Top-Rated Animated Films',
+  crime: 'Crime Movies: Uncover a World of Intrigue with Our Comprehensive List',
+  drama: 'Drama Movies: Comprehensive List of Must-Watch Films ',
+  action: 'Action Movies: Comprehensive List of Must-Watch Blockbusters',
+  family: 'Family Movies: Essential Watchlist for Quality Time Together',
+  history: 'Historical Films: Detailed List & Reviews of History Movies',
+  horror: 'Horror Movies: Unleash Your Fears with Our Ultimate List',
+  mystery: 'Mystery Movies: Unravel Thrills with Our Ultimate Film List',
+  music: 'Music Movies List 2023: Explore Best Musicals & Soundtrack Films',
+  romance: 'Romance Movies: Unforgettable Love Stories To Make Your Heart Flutter',
+  'science-fiction': 'Sci-Fi Movies List | Discover the Best of Science Fiction Cinema',
+  thriller: 'Thriller Movies: Explore Unforgettable Suspense & Mystery Films',
+  'tv-movie': 'TV-Movie Collection: Discover & Stream Exciting Titles Today!',
+  war: 'War Movies List | Epic Historical Battles & Military Films',
+  western: 'Western Movies: Unmissable Classic & Modern Western Films List',
+};
+
+export default genreTitles;
