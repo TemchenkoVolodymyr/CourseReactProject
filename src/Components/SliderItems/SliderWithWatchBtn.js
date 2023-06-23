d0f9ff7223909d3c21457f55a0a50361c4eea8a3
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-06-23 09:51:53 +0200

    dev commit

--- src/Components/SliderItems/SliderWithWatchBtn.js
+++ src/Components/SliderItems/SliderWithWatchBtn.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CircleRating from '../CircleRating/CircleRating';
-import {useNavigate} from "react-router";
-import styles from '../Button/CustomButton.module.scss'
+import { useNavigate } from 'react-router';
+import styles from '../Button/CustomButton.module.scss';
 
 const SliderWithWatchBtn = ({ name, category, id, bg, rating, displayAsPercentage }) => {
-const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  });
 
   const watchOnClickHandler = () => {
-  navigate(`/movie/${id}`)
-  }
+    navigate(`/movie/${id}`);
+  };
   return (
     <div
       style={{
         backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.8), 
-        rgba(0,0,0,0)), url(https://image.tmdb.org/t/p/original${bg})` }}>
+        rgba(0,0,0,0)), url(https://image.tmdb.org/t/p/original${bg})`
+      }}>
       <div className="userScore">
         <p>User Score</p>
         {windowWidth >= 360 && windowWidth < 600 ? <CircleRating
